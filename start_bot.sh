9f97aa4743a7cae1448cd2573f87e1922a26319c
ulliss2015 <kudo2515@gmail.com>
2025-06-17 22:03:22 +0000

    start script update

--- start_bot.sh
+++ start_bot.sh
@@ -3,15 +3,14 @@
 # Session name
 SESSION_NAME="Link2Video-bot"
 
-# File path
-#BOT_PATH="source ~/Link2Video-bot/.venv/bin/activate; cd ~/Link2Video-bot && python3 ~/Link2Video-bot/bot_main.py"
-BOT_CMD="source $BOT_DIR/.venv/bin/activate && cd $BOT_DIR && python3 bot_main.py"
-
 # Window name
 WINDOW_NAME="link2Video-bot"
 BOT_DIR="$HOME/Link2Video-bot"
 COOKIES_SOURCE="$HOME/cookies.txt"
 COOKIES_DEST="$BOT_DIR/cookies.txt"
+
+# File path
+BOT_CMD="source $BOT_DIR/.venv/bin/activate && cd $BOT_DIR && python3 bot_main.py"
 
 # Checking Cookies source
 if [ -f "$COOKIES_SOURCE" ]; then
@@ -27,6 +26,7 @@ if ! tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
 	# Create a new session and window
 	tmux new-session -d -s "$SESSION_NAME" -n "$WINDOW_NAME" bash
 fi
+
 
 # go to session and window tmux
 #tmux send-keys -t "$SESSION_NAME:$WINDOW_NAME" "$BOT_PATH" C-m
